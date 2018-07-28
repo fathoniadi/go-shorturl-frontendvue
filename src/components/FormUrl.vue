419fe0940cb244a78041da1b0c3e2d3333c106b7
fathoniadi <fathoni.adi@gmail.com>
2018-07-28 22:47:16 +0700

    fix

--- src/components/FormUrl.vue
+++ src/components/FormUrl.vue
@@ -6,7 +6,7 @@
                     variant="danger"
                     @dismissed="dismissCountDown=0"
                     @dismiss-count-down="countDownChanged">
-              <p>{{ error_message }}</p>
+              <p v-html="error_message"></p>
               <p>This alert will dismiss after {{dismissCountDown}} seconds...</p>
               <b-progress variant="danger"
                           :max="dismissSecs"
@@ -22,13 +22,13 @@
                         </div>
                         <div class="form-group row text-right" v-show="custom_link">
                             <label for="" style="" class="col-form-label">http://localhost:8081/</label>
-                            <input v-model="input_custom_link" type="text" class="form-control col-4">
+                            <input v-model="input_custom_link" @keyup.enter="doShort()" type="text" class="form-control col-4">
                         </div>
                     </div>
                     <div class="col-md-2">
-                        <b-dropdown title="Click for 'Do IT' or click dropdown for another option" id="ddown-split" @click="doShort()" split text="Do it or " size="lg" class="">
-                          <b-dropdown-item v-if="( custom_link == false )" @click="(custom_link = true)" href="#">Use Custom Link</b-dropdown-item>
-                          <b-dropdown-item v-if="( custom_link == true )" @click="(custom_link = false)" href="#">Use Random</b-dropdown-item>
+                        <b-dropdown title="Click for 'Do IT' or click dropdown for another option" id="ddown-split" @click="doShort()" split text="Do it or -> " size="lg" class="">
+                          <b-dropdown-item v-if="( custom_link == false )" @click="(input_custom_link = '', custom_link = true)" href="#">Use Custom Link</b-dropdown-item>
+                          <b-dropdown-item v-if="( custom_link == true )" @click="(input_custom_link = '', custom_link = false)" href="#">Use Random</b-dropdown-item>
                         </b-dropdown>
                     </div>
                 </div>
@@ -54,22 +54,29 @@ export default {
     doShort: function () {
       let url = this.url
       let customLink = this.input_custom_link
+      var error = false
       if (url.length <= 0) {
+        error = true
         this.error_message = 'Please input valid URL'
-        this.dismissCountDown = this.dismissSecs
-        return
       }
 
       if (this.$validator.errors.first('url')) {
+        error = true
         this.error_message = 'Please input valid URL'
-        this.dismissCountDown = this.dismissSecs
-        return
       }
 
       if (this.custom_link && !/^[a-z0-9-]+$/.test(customLink)) {
-        this.error_message = 'Please input valid custom link. Custom link only support Number, Alphabet and Dash(-)'
+        error = true
+        this.error_message = 'Please input valid custom link. <br> Custom link only support Number, Alphabet and Dash (-)'
+      }
+
+      if (customLink.length <= 4 && this.custom_link) {
+        error = true
+        this.error_message = 'Custom link must more than 4 character'
+      }
+
+      if (error) {
         this.dismissCountDown = this.dismissSecs
-        return
       }
     },
     countDownChanged (dismissCountDown) {
